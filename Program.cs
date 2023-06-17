02b0d81be3af2a5770a41b83c53f23502c3b46f8
usagiga <usagiga@usagiga.me>
2023-06-18 03:49:01 +0900

    chore: convert WindowsFormsApplication into CommandLineApplication

--- Program.cs
+++ Program.cs
@@ -6,17 +6,11 @@ using System.Windows.Forms;
 
 namespace obs_autopilot
 {
-    static class Program
+    internal class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        public static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Console.WriteLine("Hello, World!");
         }
     }
 }
